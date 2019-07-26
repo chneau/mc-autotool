b77a684ba417b635418b29d7c458540e61055803
chneau <charles63500@gmail.com>
2019-07-26 18:11:40 +0100

    fat refactor

--- src/main/java/chneau/autotool/Main.java
+++ src/main/java/chneau/autotool/Main.java
@@ -1,18 +1,15 @@
 package chneau.autotool;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.event.client.ClientTickCallback;
-import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
-import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 
 public class Main implements ClientModInitializer {
 
 	@Override
 	public void onInitializeClient() {
-		Autotool autotool = new Autotool(new SelectBest());
-		AttackBlockCallback.EVENT.register(autotool);
-		AttackEntityCallback.EVENT.register(autotool);
-		ClientTickCallback.EVENT.register(autotool);
+		(new Autotool(new SelectBest())).register();
+		(new Autofarm()).register();
+		(new Autoswap()).register();
+		(new Autoattack()).register();
 	}
 
 }
